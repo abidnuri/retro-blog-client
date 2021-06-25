1984c1f32e2d1140aab60fa19eb754e122d55bf0
Abid Nuri <mdabidnuri@gmail.com>
2021-06-25 16:23:55 +0600

    class to className and comment delete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,17 +7,17 @@ const Login = () => {
 
     console.log(watch("example"));
     return (
-        <div class="container mx-auto px-4 mt-5 flex justify-center">           
+        <div className="container mx-auto px-4 mt-5 flex justify-center">           
             <form onSubmit={handleSubmit(onSubmit)}>
             <h2 className="text-2xl text-center text-gray-200 p-6 bg-green-900">Login Here</h2> <br />
-                <label class="block py-2">
+                <label className="block py-2">
                     <input className="form-input" type="text" defaultValue="test" {...register("example")} placeholder="Enter your email..." />
                 </label>
-                <label class="block py-2">
+                <label className="block py-2">
                     <input className="form-input" type="password" {...register("exampleRequired", { required: true })} placeholder="Enter your password..." />
                     {errors.exampleRequired && <span>This field is required</span>}
                 </label>
-                <label class="block">
+                <label className="block">
                     <input className="my-6 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" type="submit" />
                 </label>
             </form>
