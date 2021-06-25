25669c749b71720910e7019845e676edb79516d2
Abid Nuri <mdabidnuri@gmail.com>
2021-06-25 12:30:25 +0600

    tailwind css design

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SingleBlog from '../SingleBlog/SingleBlog';
+
+const Blog = () => {
+    return (
+        <div>
+            <SingleBlog />
+        </div>
+    );
+};
+
+export default Blog;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div class="rounded-lg h-96 m-10 overflow-hidden">
+            <img alt="content" class="object-cover object-center h-full w-full" src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg" />
+        </div>
+    );
+};
+
+export default Header;
