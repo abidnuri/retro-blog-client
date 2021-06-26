bf9d177d7f6a2c786ec315580082d89f340ad262
Abid Nuri <mdabidnuri@gmail.com>
2021-06-26 23:07:44 +0600

    description truncate

--- src/components/SingleBlog/SingleBlog.js
+++ src/components/SingleBlog/SingleBlog.js
@@ -10,7 +10,9 @@ const SingleBlog = (props) => {
                 <div className="p-6">
                     <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                     <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{name}</h1>
-                    <p className="leading-relaxed mb-3">{description}</p>
+                    <div className="descriptionBlog" >
+                        <p className="mb-3 truncate ...">{description}</p>
+                    </div>
                     <div className="flex items-center flex-wrap ">
                         <Link to={`/post/${_id}`} className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Read More
                             <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
